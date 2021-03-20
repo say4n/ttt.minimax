b88d145a06b9f583dee60a19d5b5a3868442703f
Sayan Goswami <goswami.sayan47@gmail.com>
2021-03-20 22:05:43 +0100

    Add skeleton for ttt.

---
+++ ttt.py
@@ -0,0 +1,20 @@
+"""
+Tic Tac Toe game implementation. 
+"""
+
+class TicTacToe:
+    def __init__(self):
+        pass
+
+    def act(self, player):
+        pass
+
+    def get_empty_cells(self):
+        pass
+
+    def __check_win(self):
+        pass
+
+    def __repr__(self):
+        pass
+
